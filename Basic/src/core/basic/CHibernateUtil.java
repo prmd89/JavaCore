4456374500fcc5d8e9d94f6deae3a9fabcda9fcd
badruddin004 <badruddin004@gmail.com>
2015-06-05 19:02:48 +0530

    utility file

---
+++ Basic/src/core/basic/CHibernateUtil.java
@@ -0,0 +1,22 @@
+package Lab1;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.*;
+public class CHibernateUtil {
+static SessionFactory factory;
+static{
+	try{
+		Configuration cfg=new Configuration();
+		cfg=cfg.configure();
+		factory =cfg.buildSessionFactory();
+	}catch(Exception e){
+		e.printStackTrace();}
+	
+}
+	public static SessionFactory getSessionFactory() {
+		
+		return factory;
+	}
+
+}
